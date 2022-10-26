0042b77818a49f2203d5e7af2be6cc8363316fd4
Yoshimurka <110531276+Yoshimurka@users.noreply.github.com>
2022-10-26 19:46:22 +0300

    Little changes days

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "4814fb756a268f3ded7d52de5dd98133";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
